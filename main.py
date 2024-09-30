0c9b8d622080526066db0f5fb1982321512e6cb8
janisfs <janisfs@mail.ru>
2024-09-30 22:05:16 +0300

    add new code

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 from flask import Flask, render_template, request
 import requests
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    weather = None
+    if request.method == 'POST':
+        city = request.form['city']
+        weather = get_weather(city)
+    render_template('index.html', weather=weather)
+
+def get_weather(city):
+    api_key = "d9a0f4c37c536dec3b20825900c97115"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+    response = requests.get(url)
+    return response.json()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
